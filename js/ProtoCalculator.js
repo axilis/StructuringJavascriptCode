3ba1bfe9906a216317c84cce4a9d231e677b55c4
Fosna <vedran.marsic.ka@gmail.com>
2015-02-18 18:16:50 +0100

    Calculator got instance functions.

--- js/ProtoCalculator.js
+++ js/ProtoCalculator.js
@@ -1,16 +1,38 @@
-(function (App, $) {
+(function(App, $) {
     "use strict";
-
-    var Calculator = function() {
-        console.log("proto calculator");
+    
+    var Calculator = function(options) {
+        var settings = {
+            containerSel: "",
+            fstSel: ".fst",
+            sndSel: ".snd",
+            equalsSel: ".equals",
+            resultSel: ".result",
+        };
+        $.extend(settings, options);
+        this.settings = settings;
     };
-
+    
     Calculator.prototype = {
-        init: function () {
+        init: function() {
             console.log("proto calculator init");
+            
+            var container = $(this.settings.containerSel);
+            this.fst = container.find(this.settings.fstSel);
+            this.snd = container.find(this.settings.sndSel);
+            this.equals = container.find(this.settings.equalsSel);
+            this.result = container.find(this.settings.resultSel);
+            
+            this.equals.click(this.calculate);
+        },
+        calculate: function() {
+            var a = +this.fst.val();
+            var b = +this.snd.val();
+            var c = a + b;
+            this.result.text(c);
         }
     }
-
+    
     App.Calculator = Calculator;
 })(App, jQuery);
 
