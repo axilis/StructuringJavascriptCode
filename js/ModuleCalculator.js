479a6e7ec2d99e5144aa7d21ec3851b9b6a2a582
Fosna <vedran.marsic.ka@gmail.com>
2015-06-09 20:18:27 +0200

    Constructor in a method.

--- js/ModuleCalculator.js
+++ js/ModuleCalculator.js
@@ -1,15 +1,24 @@
 (function(App, $) {
     "use strict";
     var Calculator = function() {
-        console.log("module calculator constructor");
+        // variables
         
+        // constructor
+        var construct = function () {
+            console.log("module calculator constructor");
+        };
+
+        // methods
+
         var init = function () {
             console.log("module calculator init");
-        }
+        };
 
+        // constructor call
+        construct();
         return {
             init: init
-        }
+        };
     };
     App.Calculator = Calculator;
 })(App, jQuery);
