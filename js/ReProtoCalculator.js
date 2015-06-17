ad382b378c47f3cd6a9451caa930d6999b08f3ea
Fosna <vedran.marsic.ka@gmail.com>
2015-06-17 19:49:18 +0200

    Constructor and private variables.

--- js/ReProtoCalculator.js
+++ js/ReProtoCalculator.js
@@ -1,12 +1,24 @@
 (function(App, $) {
     "use strict";
     var Calculator = function(options) {
-        console.log("calculator constructor");
+        this.options = options;
     };
 
     Calculator.prototype = (function () {
+        var settings = {
+            containerSel: "",
+            fstSel: ".fst",
+            sndSel: ".snd",
+            equalsSel: ".equals",
+            resultSel: ".result",
+        };
+
         var init = function() {
             console.log("calculator init");
+
+            $.extend(settings, this.options);
+
+            console.log(settings);
         };
 
         return {
