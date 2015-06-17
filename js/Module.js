5c5c5982a3824af4ca78301923e984fab3ced8e6
Fosna <vedran.marsic.ka@gmail.com>
2015-06-17 19:32:29 +0200

    Cosmetic changes.

--- js/Module.js
+++ js/Module.js
@@ -2,10 +2,10 @@
     "use strict";
 
     $(function () {
-        var monkeyCalc = App.Calculator({containerSel: "#monkey"});
+        var monkeyCalc = App.Calculator({ containerSel: "#monkey" });
         monkeyCalc.init();
         
-        var cowCalc = App.Calculator({containerSel: "#cow"});
+        var cowCalc = App.Calculator({ containerSel: "#cow" });
         cowCalc.init();
     })
     
