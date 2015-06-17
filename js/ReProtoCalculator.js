8f941f5e3b967abe77fe7ad3173dcf08547b2aac
Fosna <vedran.marsic.ka@gmail.com>
2015-06-17 20:11:01 +0200

    Calculator implementation.

--- js/ReProtoCalculator.js
+++ js/ReProtoCalculator.js
@@ -5,21 +5,38 @@
     };
 
     Calculator.prototype = (function () {
-        var settings = {
+        var defaultSettings = {
             containerSel: "",
             fstSel: ".fst",
             sndSel: ".snd",
             equalsSel: ".equals",
             resultSel: ".result",
         };
+        var fst, snd, equals, result;
 
         var init = function() {
-            console.log("calculator init");
+            var _this = this;
 
-            $.extend(settings, this.options);
+            _this.settings = {};
+            $.extend(_this.settings, defaultSettings, this.options);
 
-            console.log(settings);
+            var container = $(_this.settings.containerSel);
+            _this.fst = container.find(_this.settings.fstSel);
+            _this.snd = container.find(_this.settings.sndSel);
+            _this.equals = container.find(_this.settings.equalsSel);
+            _this.result = container.find(_this.settings.resultSel);
+            
+            _this.equals.click(function () {
+                calculate(_this);
+            });
         };
+
+        var calculate = function(_this) {
+            var a = +_this.fst.val();
+            var b = +_this.snd.val();
+            var c = a + b;
+            _this.result.text(c);
+        }
 
         return {
             init: init
